baa7dec3e9cab1872e973ee11d8496794fb4702a
Lisa Nam <lsyn2016@gmail.com>
2023-06-08 14:35:48 -0400

    completed day 3

--- day 3/functions copy.py
+++
@@ -1,9 +0,0 @@
-# Functions - function, modularity, helper function, scope
-
-# create a function
-
-# call the function
-
-# scope
-
-
---
+++ day 3/nums_solution.py
@@ -0,0 +1,48 @@
+#Primitive Data Types - int & float, operations, bulit-in functions - type(), int(), float(), str()
+
+#print int
+three = 3
+
+#print float
+pie = 3.14
+
+#add/sub + -
+remainer = pie - three
+# print(remainer)
+
+#multiply * & divide /
+div = 5/3
+mult = 1.5 * 2
+# print(div)
+# print(mult)
+
+#exponentiation **
+# exp = 2 ** 4
+# print(exp)
+
+#modulus %
+# mod = 5 % 3
+# print(mod)
+def isOdd(num):
+  if num % 2 == 1:
+    print("It's odd number")
+  else:
+    print("It's not odd!")
+
+# isOdd(100)
+# isOdd(51)
+# isOdd(42)
+# isOdd(35)
+
+#floor division //
+# print(5 // 3)
+
+#type()
+# print(type(5.))
+
+#type conversion - int(), float(), str()
+# flo = int(3.54)
+# print(flo)
+
+
+
